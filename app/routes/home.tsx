771c6c80b96377655b8f1d18f2e380d6cc5512de
mideyolu <olumideoluwuyi91@gmail.com>
2025-10-18 17:18:13 +0100

    included home file

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,40 @@
+import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "SynchHire" },
+    { name: "description", content: "Smart Resume Scorer" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+    return <main className="bg-[url('/images/bg-main.png')] bg-cover">
+        <Navbar/>
+        <section className="main-section">
+            <div className="page-heading py-16">
+                <h1>Track your Resume ratings</h1>
+                <h2>Review your submissions and check AI-powered feedback.</h2>
+            </div>
+
+            <>
+                {
+                    resumes.length > 0 ? (
+                        <div className="resumes-section">
+                            {
+                                resumes.map((resume) => (
+                                    <ResumeCard key={resume.id} resume={ resume} />
+                                ))
+                            }
+
+                        </div>
+                    ) : "No resumes available"
+                }
+
+            </>
+        </section>
+
+  </main>;
 }
