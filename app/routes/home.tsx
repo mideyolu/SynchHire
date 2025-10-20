715947b4ccb7fb4999e559ae079a5c52f85a027a
mideyolu <olumideoluwuyi91@gmail.com>
2025-10-20 14:10:45 +0100

    Included home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -11,7 +11,7 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-    return <main className="bg-[url('/images/bg-main.png')] bg-cover">
+    return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
         <Navbar/>
         <section className="main-section">
             <div className="page-heading py-16">
