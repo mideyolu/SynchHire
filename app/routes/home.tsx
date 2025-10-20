d7bb72d43666690f30589832cf553b6e78fadfef
mideyolu <olumideoluwuyi91@gmail.com>
2025-10-20 14:04:04 +0100

    Revert "updates"
    This reverts commit 812c7a8154d1614e9fb95ad4624bc06958ba2b5c.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,49 +1,40 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router";
 import Navbar from "~/components/Navbar";
 import ResumeCard from "~/components/ResumeCard";
-import { usePuterStore } from "~/lib/puter";
-import { resumes } from "../../constants";
 import type { Route } from "./+types/home";
+import { resumes } from "../../constants";
 
 export function meta({}: Route.MetaArgs) {
-    return [
-        { title: "SynchHire" },
-        { name: "description", content: "Smart Resume Scorer" },
-    ];
+  return [
+    { title: "SynchHire" },
+    { name: "description", content: "Smart Resume Scorer" },
+  ];
 }
 
 export default function Home() {
-    const { isLoading, auth } = usePuterStore();
-    const navigate = useNavigate();
+    return <main className="bg-[url('/images/bg-main.png')] bg-cover">
+        <Navbar/>
+        <section className="main-section">
+            <div className="page-heading py-16">
+                <h1>Track your Resume ratings</h1>
+                <h2>Review your submissions and check AI-powered feedback.</h2>
+            </div>
 
-    useEffect(() => {
-        !auth.isAuthenticated ? navigate("/auth?next=/") : null;
-    }, [auth.isAuthenticated]);
-    return (
-        <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-            <Navbar />
-
-            <section className="main-section">
-                <div className="page-heading py-16">
-                    <h1>Track your Resume ratings</h1>
-                    <h2>
-                        Review your submissions and check AI-powered feedback.
-                    </h2>
-                </div>
-
-                <>
-                    {resumes.length > 0 ? (
+            <>
+                {
+                    resumes.length > 0 ? (
                         <div className="resumes-section">
-                            {resumes.map((resume) => (
-                                <ResumeCard key={resume.id} resume={resume} />
-                            ))}
+                            {
+                                resumes.map((resume) => (
+                                    <ResumeCard key={resume.id} resume={ resume} />
+                                ))
+                            }
+
                         </div>
-                    ) : (
-                        "No resumes available"
-                    )}
-                </>
-            </section>
-        </main>
-    );
+                    ) : "No resumes available"
+                }
+
+            </>
+        </section>
+
+  </main>;
 }
