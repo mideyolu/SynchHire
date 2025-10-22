90941b8481448403b7ec99f85824374189b3a5be
mideyolu <olumideoluwuyi91@gmail.com>
2025-10-22 14:54:31 +0100

    Fetch resumes from storage on the homepage

--- app/components/details.types.ts
+++
@@ -1,7 +0,0 @@
-export interface Tip {
-    type: "good" | "improve";
-    tip: string;
-    explanation: string;
-}
-
-export type Tips = Tip[];
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,9 +1,8 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router";
 import Navbar from "~/components/Navbar";
 import ResumeCard from "~/components/ResumeCard";
 import { usePuterStore } from "~/lib/puter";
-import { resumes } from "../../constants";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
@@ -14,12 +13,33 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-    const { auth } = usePuterStore();
+    const { auth, kv } = usePuterStore();
     const navigate = useNavigate();
+    const [resumes, setResumes] = useState<Resume[]>([]);
+    const [loadingResume, setLoadingResume] = useState<Boolean>(false);
 
     useEffect(() => {
         !auth.isAuthenticated ? navigate("/auth?next=/") : null;
     }, [auth.isAuthenticated]);
+
+    useEffect(() => {
+        const loadingResume = async () => {
+            setLoadingResume(true);
+
+            const resumes = (await kv.list("resume:*", true)) as KVItem[];
+
+            const parsedResume = resumes?.map(
+                (resume) => JSON.parse(resume.value) as Resume,
+            );
+
+            console.log("parsedResume", parsedResume);
+
+            setResumes(parsedResume || []);
+            setLoadingResume(false);
+        };
+
+        loadingResume();
+    }, []);
 
     return (
         <main className="bg-[url('/images/bg-main.svg')] bg-cover">
@@ -27,22 +47,49 @@ export default function Home() {
             <section className="main-section">
                 <div className="page-heading py-16">
                     <h3>Track your Resume ratings</h3>
-                    <h4>
-                        Review your submissions and check AI-powered feedback.
-                    </h4>
+
+                    {!loadingResume && resumes.length === 0 ? (
+                        <h4>
+                            No resumes available. Upload your first reume to get
+                            AI Feedback
+                        </h4>
+                    ) : (
+                        <h4>
+                            Review your submissions and check AI-powered
+                            feedback.
+                        </h4>
+                    )}
                 </div>
+                {loadingResume && (
+                    <div className="flex flex-col items-center justify-center">
+                        <img
+                            src="/images/resume-scan-2.gif"
+                            alt=""
+                            className="w-[200px]"
+                        />
+                    </div>
+                )}
 
                 <>
-                    {resumes.length > 0 ? (
+                    {!loadingResume && resumes.length > 0 && (
                         <div className="resumes-section">
                             {resumes.map((resume) => (
                                 <ResumeCard key={resume.id} resume={resume} />
                             ))}
                         </div>
-                    ) : (
-                        "No resumes available"
                     )}
                 </>
+
+                {!loadingResume && resumes?.length === 0 && (
+                    <div className="flex flex-col items-center justify-center mt-10 gap-4">
+                        <Link
+                            className="primary-button w-fit h-fit text-xl font-semibold"
+                            to="/upload"
+                        >
+                            Upload Resume
+                        </Link>
+                    </div>
+                )}
             </section>
         </main>
     );
